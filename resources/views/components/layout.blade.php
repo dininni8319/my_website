17eebf0410334603ef63465299cd88844c12d8e2
Salvatore Dininni <salvatoredininni@unknown021678045924.attlocal.net>
2022-01-11 18:09:11 -0800

    implemetation, favicon and title

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/app.css">
-    <title>Personal Project</title>
+    <link rel="shortcut icon" href="/logo.png" type="image/x-icon"/>
+    <title>{{$title ?? 'Salvatore Dininni'}}</title>
 </head>
 <body>
     <x-navbar />
