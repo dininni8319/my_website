313711df89f55cadb03e4667e7947b66e1b00a31
Salvatore Dininni <salvatoredininni@Salvatores-MacBook-Pro.local>
2022-01-14 11:10:38 +0100

    style responsive implementation

--- public/js/app.js
+++ public/js/app.js
@@ -2278,7 +2278,7 @@ document.addEventListener('scroll', function () {
 
   if (scrolled > 30) {
     nav.style.background = ' rgb(159,159,150)';
-    nav.style.background = ' linear-gradient(0deg, rgba(159,159,150,0.018644957983193322) 14%, rgba(255,255,255,0.938813025210084) 16%, rgba(246,245,242,0.639093137254902) 63%)';
+    nav.style.background = 'linear-gradient(0deg, rgba(159,159,150,0.018644957983193322) 10%, rgba(255,255,255,0.938813025210084) 16%, rgba(246,245,242,0.639093137254902) 63%)';
   } else {
     nav.style.backgroundColor = 'var(--backgroundColor)';
   }
