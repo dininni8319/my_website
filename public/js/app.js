8ad08d01892f150f071ff69d6528f5e88ab4e50e
Salvatore Dininni <salvatoredininni@Salvatores-MacBook-Pro.local>
2022-01-17 07:04:25 +0100

    success message impleemented

--- public/js/app.js
+++ public/js/app.js
@@ -2346,21 +2346,23 @@ if (window.matchMedia("(min-width:700px)").matches) {
   \*********************************/
 /***/ (() => {
 
-function messageSuccess(str) {
-  var message = document.querySelector("#message-sent");
-  console.log(message, 'test the getelement');
-  var h4 = document.createElement('h4');
-  h4.style.color = 'white';
-  h4.style.backgroundColor = '#2FB986';
-  h4.textContent = 'Thank you we will contact you as soon as possible';
-  return message.appendChild(h4);
-}
+var message = document.querySelector("#message-sent");
 
-console.log(messageSuccess());
-setTimeout(function () {
-  var message = document.querySelector("#message-sent");
-  message.style.display = 'none';
-}, 3000); // function messageError(str) {
+if (message) {
+  var messageSuccess = function messageSuccess(str) {
+    var h4 = document.createElement('h4');
+    h4.style.color = 'white';
+    h4.style.backgroundColor = '#2FB986';
+    h4.textContent = 'Thank you we will contact you as soon as possible';
+    return message.appendChild(h4);
+  };
+
+  document.addEventListener('DOMContentLoaded', messageSuccess);
+  setTimeout(function () {
+    var message = document.querySelector("#message-sent");
+    message.style.display = 'none';
+  }, 3000);
+} // function messageError(str) {
 //    let message = document.getElementById('message-error')
 //    console.log(about, 'test the error function');
 //    let h3 = document.createElement('h3')
--- resources/js/message.js
+++ resources/js/message.js
@@ -1,28 +1,30 @@
-function messageSuccess(str) {
-            
-    let message = document.querySelector("#message-sent")
-    console.log(message, 'test the getelement');
-    let h4 = document.createElement('h4')
-    
-    h4.style.color = 'white'
-    h4.style.backgroundColor = '#2FB986'
-    h4.textContent = 'Thank you we will contact you as soon as possible'
-    
-    return message.appendChild(h4) 
-}
-
-console.log(messageSuccess());
-
-setTimeout(() => { 
 let message = document.querySelector("#message-sent")
-message.style.display = 'none'
-}, 3000)
 
-// function messageError(str) {
+if (message) {
+    
+    function messageSuccess(str) {
 
-//    let message = document.getElementById('message-error')
-//    console.log(about, 'test the error function');
-//    let h3 = document.createElement('h3')
+            let h4 = document.createElement('h4')
+            h4.style.color = 'white'
+            h4.style.backgroundColor = '#2FB986'
+            h4.textContent = 'Thank you we will contact you as soon as possible'
+            
+            return message.appendChild(h4)       
+    }
+
+    document.addEventListener('DOMContentLoaded', messageSuccess)
+        
+    setTimeout(() => { 
+        let message = document.querySelector("#message-sent")
+        message.style.display = 'none'
+    }, 3000)
+}
+    
+    // function messageError(str) {
+        
+        //    let message = document.getElementById('message-error')
+        //    console.log(about, 'test the error function');
+        //    let h3 = document.createElement('h3')
 //    h3.style.color = 'black'
 //    h3.style.backgroundColor = 'red'
 //    h3.textContent = 'Something went wrong'
