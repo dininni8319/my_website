ec14b0e755fb6909fe16ba1dd54bb6f49151dd1a
Savatore Dininni <s.dininni@yahoo.com>
2022-02-04 12:57:32 +0100

    added project rehacktor to my website

--- resources/views/components/projects-mobile.blade.php
+++ resources/views/components/projects-mobile.blade.php
@@ -3,6 +3,17 @@
         <h3 class="row-h3-projects">My Projects</h3>
         <div class="wrapper-cards">
             <div class="card-projects">
+                <img class="card-image" src="/projects_pictures/game-app-detail-page.png" alt="1" />
+                <div class="card-descriptions">
+                    <h4 class="card-title">Rehacktor Project</h4>
+                     <a href="https://github.com/dininni8319/rehacktor" class="card-link" target="_blank">
+                        <i class="fab fa-gitlab fa-1x gitlab"></i>
+                    </a>
+                    
+                </div>
+                {{-- <p class="text-white">This project is a Clone of a famous web app Subito.it. Stack technologies involved. Dropzone.js, Laravel Spatie Media Library, Google Cloud Vision Api, Multi language, Revisor page, Laravel 8, Laravel Fortify, watermark, search full text and more...</p> --}}
+            </div>
+            <div class="card-projects">
                 <img class="card-image" src="/projects_pictures/presto.png" alt="1" />
                 <div class="card-descriptions">
                     <h4 class="card-title">Presto Project</h4>
@@ -69,21 +80,10 @@
                 {{-- <p class="text-white">This project is a Clone of a famous web app Subito.it. Stack technologies involved. Dropzone.js, Laravel Spatie Media Library, Google Cloud Vision Api, Multi language, Revisor page, Laravel 8, Laravel Fortify, watermark, search full text and more...</p> --}}
             </div>
             <div class="card-projects">
-                <img class="card-image" src="/projects_pictures/e-commerce.png" alt="1" />
-                <div class="card-descriptions">
-                    <h4 class="card-title">E-commerce</h4>
-                     <a href="https://gitlab.propulsion-home.ch/salvatore_dininni/e-commerce" class="card-link" target="_blank">
-                        <i class="fab fa-gitlab fa-1x gitlab"></i>
-                    </a>
-                    
-                </div>
-                {{-- <p class="text-white">This project is a Clone of a famous web app Subito.it. Stack technologies involved. Dropzone.js, Laravel Spatie Media Library, Google Cloud Vision Api, Multi language, Revisor page, Laravel 8, Laravel Fortify, watermark, search full text and more...</p> --}}
-            </div>
-            <div class="card-projects">
-                <img class="card-image" src="/projects_pictures/web-blog-app.png" alt="1" />
+                <img class="card-image" src="/projects_pictures/blog-app-picture.png" alt="1" />
                 <div class="card-descriptions">
                     <h4 class="card-title">Web Blog App</h4>
-                     <a href="https://gitlab.propulsion-home.ch/salvatore_dininni/food-web-app-blog" class="card-link" target="_blank">
+                    <a href="https://gitlab.propulsion-home.ch/salvatore_dininni/food-web-app-blog" class="card-link" target="_blank">
                         <i class="fab fa-gitlab fa-1x gitlab"></i>
                     </a>
                     
@@ -94,14 +94,25 @@
                 <img class="card-image" src="/projects_pictures/food-app.png" alt="1" />
                 <div class="card-descriptions">
                     <h4 class="card-title">Food Order App</h4>
-                     <a href="https://gitlab.propulsion-home.ch/salvatore_dininni/foodapp" class="card-link" target="_blank">
+                    <a href="https://gitlab.propulsion-home.ch/salvatore_dininni/foodapp" class="card-link" target="_blank">
                         <i class="fab fa-gitlab fa-1x gitlab"></i>
                     </a>
                     
                 </div>
                 {{-- <p class="text-white">This project is a Clone of a famous web app Subito.it. Stack technologies involved. Dropzone.js, Laravel Spatie Media Library, Google Cloud Vision Api, Multi language, Revisor page, Laravel 8, Laravel Fortify, watermark, search full text and more...</p> --}}
             </div>
-    
+            <div class="card-projects">
+                <img class="card-image" src="/projects_pictures/e-commerce.png" alt="1" />
+                <div class="card-descriptions">
+                    <h4 class="card-title">E-commerce</h4>
+                     <a href="https://gitlab.propulsion-home.ch/salvatore_dininni/e-commerce" class="card-link" target="_blank">
+                        <i class="fab fa-gitlab fa-1x gitlab"></i>
+                    </a>
+                    
+                </div>
+                {{-- <p class="text-white">This project is a Clone of a famous web app Subito.it. Stack technologies involved. Dropzone.js, Laravel Spatie Media Library, Google Cloud Vision Api, Multi language, Revisor page, Laravel 8, Laravel Fortify, watermark, search full text and more...</p> --}}
+            </div>
+            
         </div>
         
     </div>
